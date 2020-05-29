b958ad66783765c2041f6bde0a8051171fe1d89c
FedericaRipani <federchicca99@hotmail.com>
2020-05-29 17:13:24 +0200

    > Aggiunta Downloader

--- OOP-Poject-TwitterTimeline/src/main/java/com/exam/Service/Downloader.java
+++ OOP-Poject-TwitterTimeline/src/main/java/com/exam/Service/Downloader.java
@@ -1,5 +1,49 @@
 package com.exam.Service;
 
-public class Downloader {
+import java.io.IOException;
+import java.net.URL;
+import org.apache.commons.io.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.nio.charset.Charset;
 
+/**
+ * 
+ * Classe che permette di prendere dei dati in formato JSON da un URL,
+ * tramite il metodo DownloaderJson;
+ * 
+ * @author Federica Ripani
+ */
+
+
+public class Downloader {
+	
+	//Costruttore
+     public Downloader() {
+     }
+     
+     /**
+      * 
+      * Metodo che, dato l'URL, produce un JSONArray formato 
+      * dal JSONObject presente nel campo "statuses";
+     
+      * @return array
+      * @throws IOException
+      */
+	 public JSONArray DownloaderJson() throws IOException {
+			
+		JSONObject json_ob = new JSONObject(IOUtils.toString(
+			new URL("https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/search/tweets.json?q=terremoto&count=100//"), 
+			Charset.forName("UTF-8")));
+		
+		JSONArray array = new JSONArray(json_ob.get("statuses").toString());
+		
+		
+		
+		return array;
+	
+	}
+	
+	
 }
+	
