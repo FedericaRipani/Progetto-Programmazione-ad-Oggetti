2f46ea93251520b16930faf63748af78dcbb63bd
Camilla D'Andrea <camilladandrea@live.it>
2020-05-30 16:42:15 +0200

    Implementazione database dei metadati

--- OOP-Poject-TwitterTimeline/src/main/java/com/exam/Database/DatabaseMetadati.java
+++ OOP-Poject-TwitterTimeline/src/main/java/com/exam/Database/DatabaseMetadati.java
@@ -1,5 +1,35 @@
 package com.exam.Database;
+import java.util.ArrayList;
 
-public class DatabaseMetadati {
+import com.exam.model.*;
+
+/**
+ * Definisce un array list di Metadati
+ * @return ArrayList di oggetti Metadati
+ * 
+ * @author Camilla D'Andrea
+ */
+
+public class DatabaseMetadati
+ {
+
+	private static ArrayList<Metadati> metadata = new ArrayList<Metadati>();
 	
+	
+
+	public ArrayList<Metadati> getAll() {
+		metadata.add(new Metadati("data","Data di creazione del twitt","String"));
+		metadata.add(new Metadati("idPost","Descrive in modo univoco il post","Integer"));
+		metadata.add(new Metadati("textPost","Campo testuale del post","String"));
+		metadata.add(new Metadati("idUser","Descrive in modo univoco l'utente","Integer"));
+		metadata.add(new Metadati("nameUser","Nome dell'utente","String"));
+		metadata.add(new Metadati("numPost","Numero dei tweet di quell'utente","Integer"));
+		metadata.add(new Metadati("languagePost","Lingua del twitt","String"));
+		metadata.add(new Metadati("userMentions","Descrive gli utenti menzionati","String"));
+		metadata.add(new Metadati("hashtag","Descrive gli Hashtag impiegati nel post","String"));
+		
+		return metadata;
+	}
+
 }
+
