c361e61df985b46e130abdab9029628ca24b11fd
FedericaRipani <federchicca99@hotmail.com>
2020-06-09 23:34:57 +0200

    > Classe padre Stats OK

--- OOP-Poject-TwitterTimeline/src/main/java/com/exam/Statistic/Stats.java
+++ OOP-Poject-TwitterTimeline/src/main/java/com/exam/Statistic/Stats.java
@@ -3,15 +3,25 @@ package com.exam.Statistic;
 import java.util.Map;
 
 public class Stats {
-	
 	private Map<String, Object> M;
-
+	
+	
+	public Stats(Map<String, Object> map) {
+		this.setM(map);
+	}
+	
+	
 	public Stats() {
 	}
 	
-	public Stats(Map<String, Object> map) {
-		this.M=map;	
+
+	public Map<String, Object> getM() {
+		return M;
 	}
 	
-}
 
+	public void setM(Map<String, Object> m) {
+		M = m;
+	}
+
+}
