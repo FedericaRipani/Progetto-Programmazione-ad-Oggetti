0a8430e1fdff5c723fc52bb9fb30408dee9b28f0
FedericaRipani <federchicca99@hotmail.com>
2020-06-10 00:02:15 +0200

    > Modifiche classe astratta statistiche

--- OOP-Poject-TwitterTimeline/src/main/java/com/exam/Abstract/AbstractStatistic.java
+++ OOP-Poject-TwitterTimeline/src/main/java/com/exam/Abstract/AbstractStatistic.java
@@ -6,14 +6,11 @@ import java.util.Map;
 import com.exam.model.Tweet;
 
 public abstract class AbstractStatistic {
-	   private Map<String, Object> map;
 
 
 		public AbstractStatistic() {
 
 		}
-
-	    public abstract void Stats(Map<String, Object> map);
 
 
 		public abstract int calcola(ArrayList<Tweet> vett, String field);
