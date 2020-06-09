4cb869136489cf4b35863d14d88ca477f089c3b6
FedericaRipani <federchicca99@hotmail.com>
2020-06-09 23:29:48 +0200

    > Inizio statistiche + prova classe padre Stats

---
+++ OOP-Poject-TwitterTimeline/src/main/java/com/exam/Statistic/Stats.java
@@ -0,0 +1,17 @@
+package com.exam.Statistic;
+
+import java.util.Map;
+
+public class Stats {
+	
+	private Map<String, Object> M;
+
+	public Stats() {
+	}
+	
+	public Stats(Map<String, Object> map) {
+		this.M=map;	
+	}
+	
+}
+
