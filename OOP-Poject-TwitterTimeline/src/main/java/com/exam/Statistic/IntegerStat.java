a06e55490cc6b6e42f9a39d59764ee919f3382f7
FedericaRipani <federchicca99@hotmail.com>
2020-06-10 10:45:52 +0200

    > Aggiunta IntegerStat

---
+++ OOP-Poject-TwitterTimeline/src/main/java/com/exam/Statistic/IntegerStat.java
@@ -0,0 +1,17 @@
+package com.exam.Statistic;
+
+import java.util.ArrayList;
+
+import org.json.JSONException;
+
+import com.exam.model.Tweet;
+
+public class IntegerStat {
+	
+	private  T;
+	
+	public IntegerStat(ArrayList<Tweet> vett, String field) throws JSONException {
+		
+	}
+
+}
