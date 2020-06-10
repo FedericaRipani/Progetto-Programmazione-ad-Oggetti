4dbdef60265feff0693ec6f704f6e1fedfbf8071
pc <pc@FedericaRipani.station>
2020-06-10 10:49:05 +0200

    > Modifiche IntegerStats

--- OOP-Poject-TwitterTimeline/src/main/java/com/exam/Statistic/IntegerStat.java
+++ OOP-Poject-TwitterTimeline/src/main/java/com/exam/Statistic/IntegerStat.java
@@ -1,6 +1,8 @@
 package com.exam.Statistic;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.json.JSONException;
 
@@ -8,9 +10,12 @@ import com.exam.model.Tweet;
 
 public class IntegerStat {
 	
-	private  T;
+	private Stats T;
 	
 	public IntegerStat(ArrayList<Tweet> vett, String field) throws JSONException {
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("Field", field);
 		
 	}
 
